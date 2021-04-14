91aed944d04acf4b99a8baeb767256e3f5e6adbe
Pawel Komarnicki <pawelkomarnicki@MacBook-Air-Pawel.local>
2021-04-14 17:19:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+#zadanie1
+# sporty=["piłka nożna","siatkówka","polo"]
+# print(sporty)
+# sporty.reverse()
+# print(sporty)
+
+#zadanie 2
+# skroty ={"al.":"aleja","cd.":"ciąg dalszy","itd.":"i tak dalej","np.":"na przykład"}
+# print(skroty)
+
+#Zadanie 3
+# gry ={"diablo 2":"rpg","fifa 2021":"sportowa","heroes 3":"strategia","Call of duty":"FPP"}
+# print(len(gry))
+
+#Zadanie 4
+# zdanie = input("Podaj zdanie")
+# print(zdanie.count("a"))
+
+#Zadanie 5
+
+# import sys as system
+# system.stdout.write("podaj a")
+# a = system.stdin.readline()
+# system.stdout.write("podaj b")
+# b = system.stdin.readline()
+# system.stdout.write("podaj c")
+# c = system.stdin.readline()
+#
+# print(int(a)**int(b)+int(c))
+
+
+
+
+# #Zadanie 6
+# a=1
+# b=2
+# c=3
+# if a>b and a>b:
+#     print("Zmiena A jest najwieksza")
+# if b>a and b>c:
+#     print("Zmiena B jest najwieksza")
+# if c>a and c>b:
+#     print("zmiena c jest najwieksza")
+
+#zadanie 7
+# lista=[2,3.4,44,15.2]
+# for liczba in lista:
+#     print(liczba**2)
+
+#zadnia 8
+#lista=[1,2,3,4,5]
+#print(lista)
+
+#zadanie 9
+#lista = [1,2,3,4,5]
+#
+# for i in lista:
+#     if i % 2:
+#         print("E"*len(lista))
+#     else:
+#         print("E")
+
+
+#zadanie 10
+
+# import math
+# x=input("podaj liczbe: ")
+# x=int(x)
+# if x<=0:
+#     print("Liczba jest mniejsza od zera")
+# else:
+#     print(math.sqrt(x))
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
